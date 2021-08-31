b9ab138766da5ce5aeb93c821b717d3d41b471ea
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-08-31 16:14:56 +0900

    Blockchain 투표앱 만들기 by Ethereum
    솔리디티를 통해서 간단한 투표앱을 만들어 보았다. 손코딩으로 더 문법을 익혀보자.

--- vote.sol
+++ vote.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.4;
 
-contract Vote {
+abstract contract Vote {
     
     // structure
     struct candidator {
@@ -11,20 +11,41 @@ contract Vote {
     }
     
     // variable
+    bool live;
+    address owner;
     candidator[] public candidatorList;
 
     // mapping
+    mapping(address => bool) Voted;
     
     // event
     event AddCandidator(string name);
+    event UpVote(string candidator, uint upVote );
+    event FinishVote(bool live);
+    event Voting(address owner);
     
     // modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        // 전체 실행??
+        _;
+    }
     
     // constructor
+    // 초기값 설정 느낌??
+    constructor() {
+        owner = msg.sender;
+        live = true;
+
+        emit Voting(owner);
+    }
     
     // candidator
     // memory나 calldata를 넣어줘야만 하나???
-    function addCandidator(string memory _name) public {
+    function addCandidator(string memory _name) public onlyOwner{
+        require(live == true);
+        require(candidatorList.length < 5);
+
         candidatorList.push(candidator(_name, 0));
 
         // emit event
@@ -32,12 +53,24 @@ contract Vote {
     }
     
     // voting
-    function Voting() public {
+    function upVote(uint _indexOfCandidator) public {
+        require(live == true);
+        require(_indexOfCandidator < candidatorList.length);
+        require(Voted[msg.sender] == false);
         
+        candidatorList[_indexOfCandidator].upVote++;
+
+        Voted[msg.sender] = true;
+
+        emit UpVote(candidatorList[_indexOfCandidator].name, candidatorList[_indexOfCandidator].upVote);
     }
     
     // finish Vote
-    function finishVote() public {
-        
+    // public 이라는 말이 주인 말고도 다른 사람도 조작이 가능하나 라는 의미?
+    function finishVote() public onlyOwner{
+        live = false;
+
+        emit FinishVote(live);
     }
 }
+
