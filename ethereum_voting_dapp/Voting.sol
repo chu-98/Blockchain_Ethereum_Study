656163717b6e2257b1551492986fe0009014a305
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-09-03 15:13:06 +0900

    Voting Dapp) Three Function
    큰 틀을 짠다

---
+++ ethereum_voting_dapp/Voting.sol
@@ -0,0 +1,11 @@
+// 어떤 컴파일 버전을 이 컨트랜트에 쓸까?
+pragma solidity ^0.4.23;
+
+// class가 아닌 contract를 쓴다는 점이 다릅니다
+contract Voting {
+    // constructor to initialize candidates
+
+    // vote for candidates
+
+    // get count of votes for each candidates
+}
