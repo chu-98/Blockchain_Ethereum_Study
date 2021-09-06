4b5ea3162972e9455c0a0f4b2301071d8cb8e586
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-09-06 16:32:49 +0900

    Lottery_Dapp) Test
    왜 Test를 하는 거지???

--- Lottery_Dapp/contracts/Lottery.sol
+++ Lottery_Dapp/contracts/Lottery.sol
@@ -3,4 +3,14 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Lottery {
 
+    address public owner;
+
+    // 가장 처음 배포가 되는 함수
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function getSomeValue() public pure returns (uint256 value) {
+        return 5;
+    }
 }
---
+++ Lottery_Dapp/test/lottery.test.js
@@ -0,0 +1,19 @@
+const Lottery = artifacts.require("Lottery");
+
+contract("Lottery", function ([deployer, user1, user2]) {
+  let lottery;
+  beforeEach(async () => {
+    console.log("Before Each");
+    lottery = await Lottery.new();
+  });
+
+  it("Basic Test", async () => {
+    console.log("Basic Test");
+    let owner = await lottery.owner();
+    let value = await lottery.getSomeValue();
+
+    console.log(`owner : ${owner}`);
+    console.log(`value : ${value}`);
+    assert.equal(value, 5);
+  });
+});
