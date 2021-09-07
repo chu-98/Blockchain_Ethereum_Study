83ea28d4ebb300c009367e10fab3b4646133c1a3
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-09-07 16:22:01 +0900

    Lottery_Dapp) Dapp 설계 방법

--- Lottery_Dapp/contracts/Lottery.sol
+++ Lottery_Dapp/contracts/Lottery.sol
@@ -14,3 +14,14 @@ contract Lottery {
         return 5;
     }
 }
+
+// Dapp 설계 방법
+// 1. 지갑 관리 - Hot Wallet & Cold Wallet
+// 2. 아키텍쳐 a. smart contract - front (Data를 모두 s.c에 넣으면 유연하게 대처 X)
+//           b. smart contract - server - front (중요도에 따라 DB를 나눠서 저장할 수 있다)
+// 3. Code a. 코드 실행하는 데 돈이 든다 (디도스 공격 막을 수 있다)
+//         b. 권한 관리
+//         c. 비즈니스 로직 업데이트 (s.c의 단점 중 하나: 업데이트)
+//         d. Data Migration
+// 4. 운영 a. public - test.net -> main.net
+//        b. private
